b9d28ae584ffd3586b0f17d28a18005f1e51b513
羽 学 <1242509682@qq.com>
2024-03-18 18:14:16 +0800

    翻译

--- Implementation/PacketUtils.cs
+++ Implementation/PacketUtils.cs
@@ -7,41 +7,47 @@ using System.Threading.Tasks;
 using Terraria.Plugins.Common;
 using TShockAPI;
 
-namespace Terraria.Plugins.CoderCow.Protector {
-  public static class PacketUtils {
-    public static void SendChestItem(TSPlayer player, int chestIndex, IList<ItemData> items) {
-      const int TerrariaPacketHeaderSize = 3;
-      const int ChestItemPacketSizeNoHeader = 8;
-      const short PacketSize = TerrariaPacketHeaderSize + ChestItemPacketSizeNoHeader;
+namespace Terraria.Plugins.CoderCow.Protector
+{
+    public static class PacketUtils
+    {
+        public static void SendChestItem(TSPlayer player, int chestIndex, IList<ItemData> items)
+        {
+            const int TerrariaPacketHeaderSize = 3;
+            const int ChestItemPacketSizeNoHeader = 8;
+            const short PacketSize = TerrariaPacketHeaderSize + ChestItemPacketSizeNoHeader;
 
-      using (MemoryStream packetData = new MemoryStream(new byte[PacketSize])) {
-        BinaryWriter writer = new BinaryWriter(packetData);
+            using (MemoryStream packetData = new MemoryStream(new byte[PacketSize]))
+            {
+                BinaryWriter writer = new BinaryWriter(packetData);
 
-        // 标题
-        writer.Write(PacketSize); // 数据包大小
-        writer.Write((byte)PacketTypes.ChestItem);
+                // 标题
+                writer.Write(PacketSize); // 数据包大小
+                writer.Write((byte)PacketTypes.ChestItem);
 
-        writer.Write((short)chestIndex);
+                writer.Write((short)chestIndex);
 
-        // 为每个项目重新写入项目数据并发送数据包
-        for (int i = 0; i < items.Count; i++) {
-          ItemData item = items[i];
+                // 为每个项目重新写入项目数据并发送数据包
+                for (int i = 0; i < items.Count; i++)
+                {
+                    ItemData item = items[i];
 
-          writer.Write((byte)i);
-          writer.Write((short)item.StackSize);
-          writer.Write((byte)item.Prefix);
-          writer.Write((short)item.Type);
+                    writer.Write((byte)i);
+                    writer.Write((short)item.StackSize);
+                    writer.Write((byte)item.Prefix);
+                    writer.Write((short)item.Type);
 
-          player.SendRawData(packetData.ToArray());
+                    player.SendRawData(packetData.ToArray());
 
-          // 倒带以写入另一个项目的项目数据
-          packetData.Position -= ChestItemPacketSizeNoHeader;
+                    // 倒带以写入另一个项目的项目数据
+                    packetData.Position -= ChestItemPacketSizeNoHeader;
+                }
+            }
         }
-      }
-    }
 
-    public static void SendChestName(TSPlayer player, int chestIndex, string name) {
-      
+        public static void SendChestName(TSPlayer player, int chestIndex, string name)
+        {
+
+        }
     }
-  }
 }
--- Implementation/_Data/ProtectorChestData.cs
+++ Implementation/_Data/ProtectorChestData.cs
@@ -9,35 +9,41 @@ using Newtonsoft.Json;
 using Terraria.Plugins.Common;
 using DPoint = System.Drawing.Point;
 
-namespace Terraria.Plugins.CoderCow.Protector {
-  [JsonObject(MemberSerialization.OptIn)]
-  public class ProtectorChestData: IChest {
-    public bool IsWorldChest => false;
-    public DPoint Location { get; }
-    [JsonProperty]
-    public ItemData[] Content => this.Items.ToArray();
-    public IList<ItemData> Items { get; }
+namespace Terraria.Plugins.CoderCow.Protector
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class ProtectorChestData : IChest
+    {
+        public bool IsWorldChest => false;
+        public DPoint Location { get; }
+        [JsonProperty]
+        public ItemData[] Content => this.Items.ToArray();
+        public IList<ItemData> Items { get; }
 
-    public int Index {
-      get { throw new NotSupportedException(); }
+        public int Index
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        public string Name
+        {
+            get { return null; }
+            set { throw new NotSupportedException(); }
+        }
+
+        public ProtectorChestData(DPoint location, ItemData[] content = null)
+        {
+            this.Location = location;
+
+            if (content != null)
+                this.Items = content.Clone() as ItemData[];
+            else
+                this.Items = new ItemData[Chest.maxItems];
+        }
+
+        public ItemData[] ContentAsArray()
+        {
+            return (ItemData[])this.Content.Clone();
+        }
     }
-
-    public string Name {
-      get { return null; }
-      set { throw new NotSupportedException(); }
-    }
-
-    public ProtectorChestData(DPoint location, ItemData[] content = null) {
-      this.Location = location;
-
-      if (content != null)
-        this.Items = content.Clone() as ItemData[];
-      else
-        this.Items = new ItemData[Chest.maxItems];
-    }
-
-    public ItemData[] ContentAsArray() {
-      return (ItemData[])this.Content.Clone();
-    }
-  }
 }
